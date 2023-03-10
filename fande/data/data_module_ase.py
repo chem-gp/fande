calculation of invariants per atomic group fixed

fande/data/data_module_ase.py
@@ -62,6 +62,7 @@ class FandeDataModuleASE(LightningDataModule):
         self.test_E = None
 
         self.atomic_groups = None
+        self.n_atoms = None
 
         self.batch_size = 1_000_000
 
@@ -138,7 +139,8 @@ class FandeDataModuleASE(LightningDataModule):
             derivatives_positions=None,
             same_centers_derivatives=False,
             frames_per_batch=10,
-            train_or_test="train"):
+            train_or_test="train",
+            trajectory=None): 
         """
         Calculate SOAP invariants using librascal
 
@@ -163,8 +165,8 @@ class FandeDataModuleASE(LightningDataModule):
         --------
         X: torch.tensor
             Tensor containing the SOAP descriptors for the set of frames split by atomic groups specified in `atomic_groups`.
-        DX: torch.tensor
-            Tensor containing the derivatives of the SOAP descriptors for the set of frames split by atomic groups specified in `atomic_groups`.
+        DX: list(torch.tensor)
+            list of torch tensors containing the derivatives of the SOAP descriptors for each atomic group specified in `atomic_groups`.
         """
 
         species= soap_params['species']
@@ -213,11 +215,18 @@ class FandeDataModuleASE(LightningDataModule):
         elif train_or_test == "test":
             traj = self.traj_test
             forces = self.forces_test
+        elif trajectory is not None:
+            traj = trajectory
+            forces = None
+            raise NotImplementedError("Not implemented yet for trajectory input")
    
         for f in traj:
             f.wrap(eps=1e-18)
 
         n_atoms = len(traj[0])
+
+        if train_or_test == "train":
+            self.n_atoms = n_atoms
 
         if atomic_groups is None:
             atomic_groups = [list(range(n_atoms))]
@@ -225,10 +234,14 @@ class FandeDataModuleASE(LightningDataModule):
 
         frames_batches = self.prepare_batches(traj, forces, frames_per_batch=frames_per_batch)
 
+
+
         print(f"Total length of traj is {len(traj)}")
         print(f"Total number of batches {len(frames_batches)}")       
         print("Calculating invariants on trajectory with librascal...")
    
+        soap = SphericalInvariants(**hypers)
+
         DX_np_batched = [[] * len(frames_batches) for i in range(n_atomic_groups)]  
         F_np_batched = [[] * len(frames_batches) for i in range(n_atomic_groups)]
         grad_info_sub_batched = [[] * len(frames_batches) for i in range(n_atomic_groups)]
@@ -237,8 +250,6 @@ class FandeDataModuleASE(LightningDataModule):
             traj_b = batch['traj']
             forces_b = batch['forces']
 
-            # print("Batch loaded... Computing SOAP invariants...")
-            soap = SphericalInvariants(**hypers)
             managers = soap.transform(traj_b)
             soap_array = managers.get_features(soap)
             soap_grad_array = managers.get_features_gradient(soap)
@@ -279,20 +290,25 @@ class FandeDataModuleASE(LightningDataModule):
                     DX_np_batched[ind_ag].append(DX_np)
                     F_np_batched[ind_ag].append(forces_train_flat)
                     grad_info_sub_batched[ind_ag].append(grad_info_sub)
+
+
                 
                 if not same_centers_derivatives:
                     raise NotImplementedError("Different centers and derivatives not implemented yet")
                     indices_sub = np.where(
                     np.in1d(a%n_atoms, train_centers_positions) & 
                     np.in1d(b%n_atoms, train_derivatives_positions))[0]
-                        
-        DX_np_batched = np.array(DX_np_batched)
-        DX_np_grouped = DX_np_batched.reshape(DX_np_batched.shape[0],DX_np_batched.shape[1]*DX_np_batched.shape[2],-1)
-        F_np_batched = np.array(F_np_batched)
-        F_np_grouped = F_np_batched.reshape(F_np_batched.shape[0],F_np_batched.shape[1]*F_np_batched.shape[2])
+
+
+        DX_np_grouped = []
+        F_np_grouped = []
+        for i in range(n_atomic_groups):
+            DX_np_grouped.append( np.concatenate( DX_np_batched[i][:]) )
+            F_np_grouped.append( np.concatenate( F_np_batched[i][:]) )
         
-        DX = torch.tensor(DX_np_grouped, dtype=torch.float32).cuda()
-        F = torch.tensor(F_np_grouped, dtype=torch.float32).cuda()
+        # we cannot create a single tensor for all groups, because the number of environments is different for each group
+        DX = [torch.tensor(DX_np_grouped[i], dtype=torch.float32).cuda() for i in range(n_atomic_groups)]
+        F = [torch.tensor(F_np_grouped[i], dtype=torch.float32).cuda() for i in range(n_atomic_groups)]
 
         if train_or_test == "train":
             self.train_DX = DX
@@ -301,7 +317,12 @@ class FandeDataModuleASE(LightningDataModule):
             self.test_DX = DX
             self.test_F = F
 
-        return DX, F
+
+        if trajectory is not None:
+            return DX
+        else:
+            return
+
     
 
     def split_training_data_into_groups(self, atomic_groups):