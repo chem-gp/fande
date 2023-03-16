306ba8fca5bcbd2562b693240bf0ae588b544feb
Mikhail <tsitsvero@gmail.com>
2023-03-16 12:27:37 +0900

    environment and installation updated

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+conda create -n fande -c conda-forge python gpytorch pytorch-lightning cudatoolkit=11.3.1 ase qe xtb xtb-python dftbplus plumed dscribe ipykernel matplotlib networkx gcc clang cmake
+# torch import problems with some versions of cudatoolkit
+conda activate fande
+
+# building and installing librascal
+git clone https://github.com/lab-cosmo/librascal
+cd librascal
+mkdir build
+cd build
+cmake ..
+make
+cd ..
+pip install .
