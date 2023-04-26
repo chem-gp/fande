26976a222bf0fe0a4e19a4ba2121c7ee13283042
Misha DLBox Alpha <tsitsvero@gmail.com>
2023-04-26 18:07:11 +0900

    md install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda create -n fande -c conda-forge -y python gpytorch pytorch-lightning cudatoolkit=11.3.1 ase qe xtb xtb-python dftbplus plumed dscribe ipykernel matplotlib networkx gcc clang cmake
+conda create -n fande -c conda-forge -y python gpytorch pytorch-lightning cudatoolkit=11.3.1 ase qe xtb xtb-python dftbplus plumed dscribe ipykernel matplotlib networkx gcc clang cmake gfortran
 # torch import problems with some versions of cudatoolkit
 conda activate fande
 
@@ -14,3 +14,20 @@ cmake ..
 make
 cd ..
 pip install .
+
+# building and installing i-pi
+# https://github.com/i-pi/i-pi
+cd ..
+git clone https://github.com/i-pi/i-pi.git
+cd drivers/f90
+make
+cd ../..
+
+# echo 'export APP=123' >> ~/.bashrc #append to bashrc
+
+# Run examples:
+# cd examples/tutorial/tutorial-1/
+# i-pi tutorial-1.xml > log &
+# i-pi-driver -h localhost -p 31415 -m sg -o 15 &
+# i-pi-driver -h localhost -p 31415 -m sg -o 15 &
+# tail -f log
