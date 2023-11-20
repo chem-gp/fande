fba7e64bd9de491349b6da2cc3933abceeff5e76
Misha DLBox Alpha <tsitsvero@gmail.com>
2023-11-20 17:14:27 +0900

    ipi runner script

---
+++ fande/ipi/runner.py
@@ -0,0 +1,16 @@
+
+# To do:
+# 1. Create directory for i-pi run
+# 2. Put init.xyz and input.xml in there
+# 3. Pass the ase calculator (fande_calc)
+# 4. Run multiple clients in parallel with joblib
+# 3. Run i-pi using the input.xml
+
+
+def run_ipi(
+        ase_calc,
+        init_structure,
+        init_template = 'nvt'
+):
+    pass
+
