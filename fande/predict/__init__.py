f5f7a9021128ae27d8217714c57d2f9081489ea0
Misha DLBox Alpha <tsitsvero@gmail.com>
2023-11-24 17:19:35 +0900

    legacy predictor removed

--- fande/predict/__init__.py
+++ fande/predict/__init__.py
@@ -1,5 +1,3 @@
-# from .predictors import Predictor, SimplePredictor
+from .predictors_ase import PredictorASE 
 
-from .predictors_ase import PredictorASE #, SimplePredictorASE
 
-print("objects of fande.predict module imported...")
