33bc61b9322855f6c06d53c40b3a8e1ceb6c98b8
tsitsvero <tsitsvero@gmail.com>
2023-12-16 18:47:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-conda create -n fande -c conda-forge -y python gpytorch pytorch-lightning cudatoolkit=11.3.1 ase qe xtb xtb-python dftbplus plumed py-plumed dscribe ipykernel matplotlib networkx gcc clang cmake gfortran
+# conda create -n fande -c conda-forge -y python gpytorch pytorch-lightning cudatoolkit=11.3.1 ase qe xtb xtb-python dftbplus plumed py-plumed dscribe ipykernel matplotlib networkx clang cmake gfortran
 # torch import problems with some versions of cudatoolkit
-conda activate fande
 
-# building and installing librascal
-cd ..
-# git clone https://github.com/lab-cosmo/librascal
-git clone https://github.com/chem-gp/librascal.git
-cd librascal
-mkdir build
-cd build
-cmake ..
-make
-cd ..
-pip install .
+conda create -n env -c conda-forge python=3.8 cmake clang pytorch gpytorch numpy scipy ase
+
+conda activate fande
+#to install librascal DO NOT INSTALL GCC WITHIN CONDA ENVIRONMENT! Aslo build does not work with Python 3.11, 3.12
+
+pip install git+https://github.com/lab-cosmo/librascal
+
+pip install git+https://github.com/chem-gp/fande
+
 
 # building and installing i-pi
-# https://github.com/i-pi/i-pi
-cd ..
-# git clone https://github.com/i-pi/i-pi.git
 git clone https://github.com/chem-gp/i-pi.git
 cd drivers/f90
 make
 cd ../..
 
-# echo 'export APP=123' >> ~/.bashrc #append to bashrc
-
-# Run examples:
-# cd examples/tutorial/tutorial-1/
-# i-pi tutorial-1.xml > log &
-# i-pi-driver -h localhost -p 31415 -m sg -o 15 &
-# i-pi-driver -h localhost -p 31415 -m sg -o 15 &
-# tail -f log
