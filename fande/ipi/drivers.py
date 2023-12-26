59edd85caa1ccddf49a303bf9f81e71254f41c7c
Mikhail <tsitsvero@gmail.com>
2023-12-27 04:08:28 +0900

    wrapper added

--- fande/ipi/drivers.py
+++ fande/ipi/drivers.py
@@ -69,3 +69,28 @@ def make_fande_client(calc_dir, idx, atoms, ipi_port, model_file):
 
 
 
+from ase import Atoms
+from ase import io
+
+class FandeAtomsWrapper(Atoms):   
+    def __init__(self, *args, **kwargs):
+        super(FandeAtomsWrapper, self).__init__(*args, **kwargs)      
+        self.calc_history_counter = 0
+        self.request_variance = False
+    
+    def get_forces_variance(self):
+        forces_variance = super(FandeAtomsWrapper, self).calc.get_forces_variance(self)
+        return forces_variance
+
+    def get_forces(self):       
+        forces = super(FandeAtomsWrapper, self).get_forces()
+        if self.request_variance:
+            forces_variance = super(FandeAtomsWrapper, self).calc.get_forces_variance(self)
+            self.arrays['forces_variance'] = forces_variance
+        # energy = super(AtomsWrapped, self).get_potential_energy()
+
+        os.makedirs("calc_history" , exist_ok=True)
+        io.write( "ase_calc_history/" + str(self.calc_history_counter) + ".xyz", self, format="extxyz")
+        # self.calc_history.append(self.copy())       
+        self.calc_history_counter += 1
+        return forces
